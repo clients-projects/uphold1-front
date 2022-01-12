652fa30f01920875f86f08c7b9aad1864be838ac
Igboanugwo Collins <munisco12@gmail.com>
2022-01-12 22:18:59 +0100

    working on login form

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,15 @@
 import React from 'react'
+import Form from '../components/Form'
 
-import Main from '../components/Main'
 import '../styles/styles.css'
 
 function Layout() {
     return (
         <div className='h-screen overflow-x-hidden justify-items-center'>
             <div className='md:grid w-full md:mx-auto h-full'>
-                
                 <div className=' my-9 justify-self-center md:w-4/12'>
-                    <Main />
+                    <Form />
                 </div>
-               
             </div>
         </div>
     )
