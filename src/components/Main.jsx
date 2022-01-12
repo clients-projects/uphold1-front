a5d9382ceeb7ec830f6c855c9d71dee81fe51151
Igboanugwo Collins <munisco12@gmail.com>
2022-01-12 21:54:30 +0100

    working on login form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,6 @@ import Form from './Form'
 
 
 import '../styles/styles.css'
-import LogoWhite from '../assets/logo-white.svg'
 
 
 const Main = () => {
@@ -11,8 +10,6 @@ const Main = () => {
     return (
         <div className='grid '>
             <div className=' grid'>
-
-                <img src={LogoWhite} alt='' className='lg:w-1/4 w-5/12 justify-self-center my-7 '/>
               
                 <div className='mx-4'>
                     <Form />
