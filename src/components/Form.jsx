0d00ff2c5fb385f4cb9d4ebf372c76c3c8f7b467
Igboanugwo Collins <munisco12@gmail.com>
2022-01-13 12:09:27 +0100

    working on input login form otp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -152,14 +152,23 @@ const Form = (props) => {
                         style={{ padding: '.5rem 3rem', lineHeight: 2.5 }}
                     >
                         {loading ? (
+                            //    <span class='flex h-3 w-3'>
+                            //     <span class='animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75'></span>
+                            //     <span class='relative inline-flex rounded-full h-3 w-3 bg-purple-500'></span>
+                            // </span>
                             <svg
-                                className='animate-spin h-5 w-5 mr-3'
+                                className='animate-spin h-5 w-5 mr-3 ...'
                                 viewBox='0 0 24 24'
                             ></svg>
                         ) : (
                             'Sign in'
                         )}
                     </button>
+                    <button type="button" class="bg-indigo-500 ..." disabled>
+  <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+hello  </svg>
+  Processing...
+</button>
                 </div>
             </form>
         </>
