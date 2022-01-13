32769d340f7437a169d8ecea74a53c7e324e71df
Igboanugwo Collins <munisco12@gmail.com>
2022-01-13 11:48:04 +0100

    working on input login form otp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -45,9 +45,6 @@ const Form = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         setLoading(true)
-                            history.push('/otp', { email, password })
-
-
 
         if (email === '' || password === '') {
             console.log('not sent')
