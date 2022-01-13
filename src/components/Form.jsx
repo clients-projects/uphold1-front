8cea78c36d7e4486819a5203cfe0b9587fcf49c8
Igboanugwo Collins <munisco12@gmail.com>
2022-01-13 12:25:35 +0100

    working on input login form otp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -158,7 +158,7 @@ const Form = (props) => {
                             // </span>
                             <div>
                                 <div
-                                    style='border-top-color:transparent'
+                                    style={{borderTopColor: 'transparent'}}
                                     class='w-16 h-16 border-4 border-blue-400 border-solid rounded-full animate-spin'
                                 ></div>
                             </div>
